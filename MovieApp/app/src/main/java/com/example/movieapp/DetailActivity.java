bb1ef14120161aa430dbc494a26d54d2f2e76d8d
tan <haitanle88@gmail.com>
2019-09-12 12:38:47 -0400

    working cleanup

--- MovieApp/app/src/main/java/com/example/movieapp/DetailActivity.java
+++ MovieApp/app/src/main/java/com/example/movieapp/DetailActivity.java
@@ -1,28 +1,21 @@
 package com.example.movieapp;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ActionBar;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Network;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.movieapp.database.FavoriteDao;
 import com.example.movieapp.database.FavoriteEntry;
 import com.example.movieapp.database.FavoriteRoomDatabase;
 import com.example.movieapp.model.Movie;
@@ -39,6 +32,7 @@ import java.util.ArrayList;
 public class DetailActivity extends AppCompatActivity implements TrailerAdapter.ListItemClickListener{
 
     private final String URL_MOVIE_POSTER_SMALL = "https://image.tmdb.org/t/p/w185//";
+    //todo: fix trailer query part1/2
     private final String TRAILER_QUERY_PATH_PT1 = "https://api.themoviedb.org/3/movie/";
     private final String TRAILER_QUERY_PATH_PT2 = "/videos?language=en-US&api_key=e4da10679254ee5d37b6f371a66acccf";
 
@@ -47,7 +41,6 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
     private ImageView mPosterImageView;
     private TextView mRatingTextView;
     private TextView mReleaseDateTextView;
-    private TextView mTrailerTv;
 
     private TrailerAdapter mTrailerAdapter;
     private RecyclerView mTrailerList;
@@ -57,11 +50,10 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
 
     private ImageView mStarImageV;
 
-    private FavoriteViewModel favoriteViewModel;
-
     private FavoriteViewModel mFavoriteViewModel;
-
     private AddFavoriteViewModel viewModel;
+
+    private AddFavoriteViewModelFactory factory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +63,7 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
         Intent intent = getIntent();
         final Movie movie = (Movie) intent.getSerializableExtra(Intent.EXTRA_TEXT);
 
+        //todo: create function to set DetailActivity UI
         mTitleTextView = (TextView) findViewById(R.id.tv_movieTitle);
         mTitleTextView.setText(movie.getTitle());
 
@@ -86,13 +79,13 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
         mReleaseDateTextView = (TextView) findViewById(R.id.tv_release_date);
         mReleaseDateTextView.setText(movie.getReleaseDate());
 
+        //todo: fix trailer/review reuse code
         makeTrailerUri(movie.getMovieApiID());
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mTrailerList = (RecyclerView) findViewById(R.id.trailer_list_recyclerView);
         mTrailerList.setLayoutManager(layoutManager);
         mTrailerList.setHasFixedSize(true);
-
         mTrailerAdapter = new TrailerAdapter(this);
         mTrailerList.setAdapter(mTrailerAdapter);
 
@@ -102,56 +95,25 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
         mReviewList = (RecyclerView) findViewById(R.id.review_list_recyclerView);
         mReviewList.setLayoutManager(layoutManager);
         mReviewList.setHasFixedSize(true);
-
         mReviewAdapter = new ReviewAdapter();
         mReviewList.setAdapter(mReviewAdapter);
 
+        //there has to be some easy way to tie this into the liveData selector
         mStarImageV = (ImageView) findViewById(R.id.star_image_view);
 
         mFavoriteViewModel = ViewModelProviders.of(this).get(FavoriteViewModel.class);
 
-//        mFavoriteViewModel.getAllFavorites().observe(this, new Observer<List<FavoriteEntry>>() {
-//            @Override
-//            public void onChanged(@Nullable List<FavoriteEntry> favoriteEntries) {
-//               for (FavoriteEntry favoriteEntry: favoriteEntries){
-//                   if (favoriteEntry.getApiId().equals(movie.getMovieApiID())){
-//                       mStarImageV.setSelected(true);
-//                       return;
-//                   }
-//               }
-//               mStarImageV.setSelected(false);
-//            }
-//        });
 
         final FavoriteRoomDatabase mdb = FavoriteRoomDatabase.getDatabase(getApplication());
 
-//        favoriteViewModel = ViewModelProviders.of(DetailActivity.this).get(FavoriteViewModel.class);
-//
-//        favoriteViewModel.insert(new FavoriteEntry(movie.getMovieApiID(),movie.getTitle()));
-//
-//        LiveData<FavoriteEntry> favoriteEntry = favoriteViewModel.getMovieById(movie.getMovieApiID());
-//
-//        favoriteEntry.observe(this, new Observer<FavoriteEntry>() {
-//
-//            @Override
-//            public void onChanged(@final FavoriteEntry favoriteMovie) {
-//
-//                mReleaseDateTextView.setText(favoriteMovie.getMovieTitle());
-////                if (favoriteMovie == null){
-////                    mStarImageV.setSelected(false);
-////                }else{
-////                    mStarImageV.setSelected(true);
-////                }
-//            }
-//        });
+        //sommme reallly badddd code
+        /*
 
-//        if (favoriteEntry == null){
-//            mStarImageV.setSelected(false);
-//        }else{
-//            mStarImageV.setSelected(true);
-//        }
 
-        AddFavoriteViewModelFactory factory = new AddFavoriteViewModelFactory(mdb,movie.getMovieApiID());
+
+         */
+
+        factory = new AddFavoriteViewModelFactory(mdb,movie.getMovieApiID());
         viewModel = ViewModelProviders.of(this, factory).get(AddFavoriteViewModel.class);
 
         viewModel.getFavorite().observe(this, new Observer<FavoriteEntry>() {
@@ -165,52 +127,22 @@ public class DetailActivity extends AppCompatActivity implements TrailerAdapter.
             }
         });
 
-//        FavoriteEntry favoriteEntry = mFavoriteViewModel.retrievebyId(movie.getMovieApiID());
-//        mFavoriteViewModel.delete(favoriteEntry);
 
         mStarImageV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mStarImageV.setBackground(getResources().getDrawable(R.drawable.star_selector));
-                // mStarImageV.setSelected(true);
-//                Intent replyIntent = new Intent(DetailActivity.this, MainActivity.class);
-//                String movieTitle = "Home Alone 2";
-//
-//                replyIntent.putExtra("Movie",movieTitle);
-//                setResult(RESULT_OK, replyIntent);
-
-                //favoriteViewModel = ViewModelProviders.of(DetailActivity.this).get(FavoriteViewModel.class);
-                //LiveData<FavoriteEntry> favoriteEntry = favoriteViewModel.getMovieById(movie.getMovieApiID());
 
                 ImageView favoriteIcon = (ImageView) findViewById(R.id.star_image_view);
                 if (favoriteIcon.isSelected()){
 
-                    //AddFavoriteViewModelFactory factory = new AddFavoriteViewModelFactory(mdb,movie.getMovieApiID());
                     FavoriteEntry favoriteEntry = viewModel.getFavorite().getValue();
-
                     mFavoriteViewModel.delete(favoriteEntry);
-                    //favoriteIcon.setSelected(false);
+
                 }else{
                     FavoriteEntry favoriteMovie = new FavoriteEntry(movie.getMovieApiID(), movie.getTitle());
                     mFavoriteViewModel.insert(favoriteMovie);
                 }
 
-
-
-
-
-
-                //mStarImageV.setSelected(true);
-
-//                if (mStarImageV.isSelected()){
-//                    mStarImageV.setSelected(false);
-//                }else{
-//                    mStarImageV.setSelected(true);
-//                }
-
-                // favoriteViewModel.insert(new FavoriteEntry(movie.getMovieApiID(),movie.getTitle()));
-
-                //startActivity(replyIntent);
             }
         });
 
