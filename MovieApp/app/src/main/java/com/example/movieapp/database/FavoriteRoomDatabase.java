c7fd058f723c0569cfe6547b1fd7db10b9222817
Tan Le <tanle@Tans-MacBook-Pro.local>
2019-12-05 11:43:15 -0500

    cleanup

--- MovieApp/app/src/main/java/com/example/movieapp/database/FavoriteRoomDatabase.java
+++ MovieApp/app/src/main/java/com/example/movieapp/database/FavoriteRoomDatabase.java
@@ -50,10 +50,6 @@ public abstract class FavoriteRoomDatabase extends RoomDatabase {
         @Override
         protected Void doInBackground(final Void... params){
             mDao.deleteAll();
-//            FavoriteEntry favoriteEntry = new FavoriteEntry("1234", "Home Alone 3");
-//            mDao.insertFavorite(favoriteEntry);
-//            favoriteEntry = new FavoriteEntry("2342", "Test test");
-//            mDao.insertFavorite(favoriteEntry);
             return null;
         }
     }
